c6616a074045cf76653750bc374d876badda1d93
Air <57860092+Air75@users.noreply.github.com>
2020-03-16 21:54:00 +0800

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,5 +1,3 @@
-import java.io.DataInput;
-
 //import javax.swing.plaf.basic.BasicInternalFrameTitlePane.CloseAction;
 
 // public class Test{
@@ -666,48 +664,93 @@ import java.io.DataInput;
 //   }
 // }
 
-public class Test {//多态
+// public class Test {//多态
+//   public static void main(String[] args) {
+
+//     /*
+//     1.Animal和Cat之间存在继承关系，Animal是父类，Cat是子类
+//     2.Cat is Animal
+//     3.new Cat()创建的对象的类型是Cat，animal2这个引用的数据类型是Animal，可见他们进行了类型转换
+//     4.子类转换成父类型，称为向上转换/upcastint，或者称为自动类型转换。
+//     5.Java中允许这种语法：父类型引用指向子类型对象。
+//     */
+
+//     Animal animal2 = new Bird();
+
+//     /*
+//     1. Java程序永远分为编译阶段和运行阶段。
+//     2. 先分析编译阶段，再分析运行阶段，编译无法通过，根本无法运行。
+//     3. 编译阶段编译器检查animal2这个引用的数据类型为Animal，由于Animal.class字节码当中有move()方法，所以编译通过了，这个过程称为静态绑定，编译阶段绑定，只有静态绑定成功之后才有后面的进行。
+//     4. 在程序运行阶段，JVM内存中真实创建的对象是Bird对象，那么以下程序在运行阶段一定会调用Cat对象的move()方法，此时发生了程序的动态绑定，运行阶段绑定。
+//     5. 无论Bird类有没有重写方法，运行阶段一定调用的是Bird对象的move方法，因为底层真实对象就是Bird对象。
+//     6. 父类型引用指向子类型对象这种机制导致程序存在编译阶段绑定和运行阶段绑定两种不同的形态/状态，这种机制可以称为一种多态语法机制。
+//     */
+
+//     animal2.move();//鸟在飞
+//     //animal2.eat();//报错
+//     Bird bird = (Bird)animal2;//强制类型转换
+//     bird.eat();
+//     Animal a3 = new Bird();
+//     /*
+//       1.以下程序编译没有问题，因为编译器检查到a3的数据类型是Animal，Animal和Cat之间存在继承关系，并且Animal是父类型，Cat是子类型，父类型转换成子类型叫做向下转换，语法合格
+//       2.程序虽然编译通过了，但是程序在运行阶段会出现异常，因为JVM堆内存中真实存在的对象是Bird类型，Bird对象无法转换为Cat对象，因为两种类型之间不存在任何继承关系，此时出现著名的异常：
+//     */
+//     //Cat c3 = (Cat)a3;
+//     if(a3 instanceof Cat){
+//       Cat c3 = (Cat)a3;
+//       c3.move();
+//     }
+//     else if(a3 instanceof Bird){
+//       Bird b3 = (Bird)a3;
+//       b3.eat();
+//     }
+//   }
+// }
+
+// class Animal {
+//   public void move() {
+//     System.out.println("动物在移动");
+//   }
+// }
+
+// class Cat extends Animal {
+
+// }
+
+// class Bird extends Animal {
+//   public void move() {
+//     System.out.println("鸟在飞");
+//   }
+//   public void eat(){
+//     System.out.println("鸟吃虫子");
+//   }
+// }
+
+public class Test {
   public static void main(String[] args) {
-
-    /*
-    1.Animal和Cat之间存在继承关系，Animal是父类，Cat是子类
-    2.Cat is Animal
-    3.new Cat()创建的对象的类型是Cat，animal2这个引用的数据类型是Animal，可见他们进行了类型转换
-    4.子类转换成父类型，称为向上转换/upcastint，或者称为自动类型转换。
-    5.Java中允许这种语法：父类型引用指向子类型对象。
-    */
-
-    Animal animal2 = new Bird();
-    
-    /*
-    1. Java程序永远分为编译阶段和运行阶段。
-    2. 先分析编译阶段，再分析运行阶段，编译无法通过，根本无法运行。
-    3. 编译阶段编译器检查animal2这个引用的数据类型为Animal，由于Animal.class字节码当中有move()方法，所以编译通过了，这个过程称为静态绑定，编译阶段绑定，只有静态绑定成功之后才有后面的进行。
-    4. 在程序运行阶段，JVM内存中真实创建的对象是Bird对象，那么以下程序在运行阶段一定会调用Cat对象的move()方法，此时发生了程序的动态绑定，运行阶段绑定。
-    5. 无论Bird类有没有重写方法，运行阶段一定调用的是Bird对象的move方法，因为底层真实对象就是Bird对象。
-    6. 父类型引用指向子类型对象这种机制导致程序存在编译阶段绑定和运行阶段绑定两种不同的形态/状态，这种机制可以称为一种多态语法机制。
-    */
-    
-    animal2.move();//鸟在飞
-    //animal2.eat();//报错
+    Master master = new Master();
+    Cat tom = new Cat();
+    master.feed(tom);
+    Dog Jary = new Dog();
+    master.feed(Jary);
   }
 }
 
-class Animal {
-  public void move() {
-    System.out.println("动物在移动");
+class Master {
+  public void feed(Pet pet) {
+    pet.eat();
   }
 }
-
-class Cat extends Animal {
-
-}
-
-class Bird extends Animal {
-  public void move() {
-    System.out.println("鸟在飞");
+class Pet{
+  public void eat(){}
   }
+class Cat extends Pet{
+  public void eat() {
+    System.out.println("小猫吃鱼！");
+  }
+}
+class Dog extends Pet{
   public void eat(){
-    System.out.println("鸟吃虫子");
+    System.out.println("小狗啃骨头！");
   }
 }
